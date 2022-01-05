8e442d073982af3f53cf459ba94642e3fff56554
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-05 17:43:43 -0600

    Make loom only for fabric module, remove minecraft code from common

--- common/src/main/java/com/github/sxp/java/api/minecraft/item/XItemStack.java
+++ common/src/main/java/com/github/sxp/java/api/minecraft/item/XItemStack.java
@@ -1,75 +1,15 @@
 package com.github.sxp.java.api.minecraft.item;
 
 import com.github.sxp.java.impl.wrapper.ItemStackWrapper;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.nbt.NbtString;
-import net.minecraft.text.Text;
 
 
 /**
  * An abrstraction of a Minecraft ItemStack
  **/
-public class XItemStack extends ItemStackWrapper {
-    public XItemStack(ItemStack stack) {
-        super(stack);
+public class XItemStack {
+    public XItemStack(ItemStackWrapper stack) {
+
     }
 
-    /**
-     * Gets the item's lore
-     * @return The lore of the item
-     **/
-    public NbtList getLore() {
-        NbtCompound nbt = this.getNbt();
-        NbtCompound display = new NbtCompound();
-        if (nbt.contains("display"))
-            display = nbt.getCompound("display");
-        if (display.contains("Lore")) return display.getList("Lore",8);
-        return new NbtList();
-    }
 
-    /**
-     * Sets the lore of the item
-     * @param lore The lore to set
-     **/
-    public void setLore(NbtList lore) {
-        NbtCompound nbt = this.getNbt();
-        NbtCompound display = new NbtCompound();
-        if (nbt.contains("display"))
-            display = nbt.getCompound("display");
-        display.put("Lore",lore);
-        nbt.put("display",display);
-    }
-
-    /**
-     * Appends a line of lore to the item
-     * @param lore The line to add
-     **/
-    public void addLoreLine(Text lore) {
-        NbtList list = getLore();
-        list.add(NbtString.of(Text.Serializer.toJson(lore)));
-        setLore(list);
-    }
-
-    /**
-     * Sets a line of lore
-     * @param line The line number to change
-     * @param lore The lore to set
-     **/
-    public void setLoreLine(int line, Text lore) {
-        NbtList list = getLore();
-        list.add(line, NbtString.of(Text.Serializer.toJson(lore)));
-        setLore(list);
-    }
-
-    /**
-     * Gets a line of lore
-     * @param line The line to get
-     * @return The line of lore
-     **/
-    public Text getLoreLine(int line) {
-        NbtList list = getLore();
-        return Text.Serializer.fromJson(list.get(line).asString());
-    }
 }
--- common/src/main/java/com/github/sxp/java/impl/wrapper/ItemStackWrapper.java
+++ common/src/main/java/com/github/sxp/java/impl/wrapper/ItemStackWrapper.java
@@ -1,130 +1,28 @@
 package com.github.sxp.java.impl.wrapper;
 
 import com.github.sxp.java.api.minecraft.item.XItemStack;
-import com.google.common.collect.Multimap;
-import net.minecraft.block.BlockState;
-import net.minecraft.client.item.TooltipData;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.tag.Tag;
-import net.minecraft.text.Text;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+public interface ItemStackWrapper {
 
-public class ItemStackWrapper {
-    private final ItemStack itemStack;
+    boolean isEmpty();
 
-    public ItemStackWrapper(ItemStack stack) {
-        itemStack = stack;
-    }
+    XItemStack split(int amount);
 
-    protected ItemStack getItemStack() {
-        return itemStack;
-    }
+    int getMaxCount();
 
-    public Item getItem() {
-        return itemStack.getItem();
-    }
+    boolean isStackable();
 
-    public static XItemStack fromNbt(NbtCompound nbt) {
-        return new XItemStack(ItemStack.fromNbt(nbt));
-    }
+    boolean isDamageable();
 
-    public Optional<TooltipData> getTooltipData() {
-        return itemStack.getTooltipData();
-    }
+    boolean isDamaged();
 
-    public boolean isEmpty() {
-        return itemStack.isEmpty();
-    }
+    int getDamage();
 
-    public XItemStack split(int amount) {
-        return new XItemStack(itemStack.split(amount));
-    }
+    void setDamage(int damage);
 
-    public boolean isIn(Tag<Item> tag) {
-        return itemStack.isIn(tag);
-    }
+    int getMaxDamage();
 
-    public boolean isOf(Item item) {
-        return  itemStack.isOf(item);
-    }
+    int getCount();
 
-    public float getMiningSpeedMultiplier(BlockState state) {
-        return itemStack.getMiningSpeedMultiplier(state);
-    }
-
-    public NbtCompound getNbt() {
-        return itemStack.getOrCreateNbt();
-    }
-
-    public void setNbt(NbtCompound nbt) {
-        itemStack.setNbt(nbt);
-    }
-
-    public int getMaxCount() {
-        return itemStack.getMaxCount();
-    }
-
-    public boolean isStackable() {
-        return itemStack.isStackable();
-    }
-
-    public boolean isDamageable() {
-        return itemStack.isDamageable();
-    }
-
-    public boolean isDamaged() {
-        return itemStack.isDamaged();
-    }
-
-    public int getDamage() {
-        return itemStack.getDamage();
-    }
-
-    public void setDamage(int damage) {
-        itemStack.setDamage(damage);
-    }
-
-    public int getMaxDamage() {
-        return itemStack.getMaxDamage();
-    }
-
-    public NbtList getEnchantments() {
-        return itemStack.getEnchantments();
-    }
-
-    public Text getName() {
-        return itemStack.getName();
-    }
-
-    public void setName(Text name) {
-        itemStack.setCustomName(name);
-    }
-
-    public void removeName() {
-        itemStack.removeCustomName();
-    }
-
-    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
-        return itemStack.getAttributeModifiers(slot);
-    }
-
-    public void addAttributeModifier(EntityAttribute attribute, EntityAttributeModifier modifier, @Nullable EquipmentSlot slot) {
-        itemStack.addAttributeModifier(attribute,modifier,slot);
-    }
-
-    public int getCount() {
-        return itemStack.getCount();
-    }
-
-    public void setCount(int count) {
-        itemStack.setCount(count);
-    }
+    void setCount(int count);
 }
