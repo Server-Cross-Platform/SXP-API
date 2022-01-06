54ce56010178c3c8ed8e19673fa41ddd015bcc1e
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-06 16:11:34 -0600

    Fix typo

--- common/src/main/java/com/github/sxp/java/impl/error/InvalidItemStackException.java
+++ common/src/main/java/com/github/sxp/java/impl/error/InvalidItemStackException.java
@@ -2,6 +2,6 @@ package com.github.sxp.java.impl.error;
 
 public class InvalidItemStackException extends RuntimeException {
     public InvalidItemStackException() {
-        super("Unable to create ItemStack, passed XItemStack into contructor.");
+        super("Unable to create ItemStack, passed XItemStack into constructor.");
     }
 }
