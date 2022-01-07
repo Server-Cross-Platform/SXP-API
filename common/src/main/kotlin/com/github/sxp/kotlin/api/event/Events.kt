deb23c4328392ed447293179e5bb5f6999b61e95
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-06 19:53:03 -0600

    owo 2: electric booguwu

--- common/src/main/kotlin/com/github/sxp/kotlin/api/event/Events.kt
+++ common/src/main/kotlin/com/github/sxp/kotlin/api/event/Events.kt
@@ -2,6 +2,14 @@ package com.github.sxp.kotlin.api.event
 
 import com.github.sxp.java.api.event.EventContext
 import com.github.sxp.java.api.event.XEvent
+import java.util.function.Consumer
 
-class Events<T:EventContext>(val event: XEvent<T>)
+class Events<T:EventContext>(val event: XEvent<T>) {
+    fun register(consumer: Consumer<T>) {
+        event.addListener(consumer)
+    }
+    fun execute(ctx: T) {
+        event.trigger(ctx)
+    }
+}
 val PLAYER_JOIN = Events<PlayerJoinEvent>(XEvent())
