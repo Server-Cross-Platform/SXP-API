4ab1782b2ace81e0ccc8d756d4bfe23436c7e5a3
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-01-06 19:29:32 -0600

    owo

--- common/src/main/kotlin/com/github/sxp/kotlin/api/event/PlayerJoinEvent.java
+++
@@ -1,9 +0,0 @@
-package com.github.sxp.kotlin.api.event;
-
-import com.github.sxp.java.api.event.EventContext;
-
-public class PlayerJoinEvent extends EventContext {
-    public PlayerJoinEvent() {
-        super("Player Join", 0);
-    }
-}
---
+++ common/src/main/kotlin/com/github/sxp/kotlin/api/event/PlayerJoinEvent.kt
@@ -0,0 +1,5 @@
+package com.github.sxp.kotlin.api.event
+
+import com.github.sxp.java.api.event.EventContext
+
+class PlayerJoinEvent : EventContext("Player Join", 0)
